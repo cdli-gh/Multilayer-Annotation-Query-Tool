a86192aa3b9f7db8d3e8493cc9ebd10524f7bc6e
sagar-sehgal <sagar.r16@iiits.in>
2019-06-02 14:42:16 +0530

    basic web interface ... form->cqp

---
+++ js/index.js
@@ -0,0 +1,65 @@
+var query="";
+function add_word(button){
+	word=`
+	<hr>
+	<div class="word">
+		<div class="word_property_list">
+			<select class="property_name">
+				<option value="None" selected deactivated>Feature</option>
+				<option value="conll:FORM">conll:FORM</option>
+				<option value="conll:LEMMA">conll:LEMMA</option>
+				<option value="conll:UPOSTAG">conll:UPOSTAG</option>
+				<option value="conll:XPOSTAG">conll:XPOSTAG</option>
+				<option value="conll:FEATS">conll:FEATS</option>
+				<option value="conll:DEPREL">conll:DEPREL</option>	
+			</select>
+			&emsp;<input type="text" class="property_value">	
+			<br>	
+		</div>
+		<button type="submit" onclick="add_property(this)">Add Property</button>
+	</div>
+	`;
+	document.getElementById('word_list').innerHTML+=word;
+}
+function add_property(block){
+	var properties = block.parentElement.getElementsByClassName("word_property_list")[0];
+	var property_select_option=`
+	<select class="property_name">
+		<option value="None" selected deactivated>Feature</option>
+		<option value="conll:FORM">conll:FORM</option>
+		<option value="conll:LEMMA">conll:LEMMA</option>
+		<option value="conll:UPOSTAG">conll:UPOSTAG</option>
+		<option value="conll:XPOSTAG">conll:XPOSTAG</option>
+		<option value="conll:FEATS">conll:FEATS</option>
+		<option value="conll:DEPREL">conll:DEPREL</option>	
+	</select>
+	&emsp;<input type="text" class="property_value">	
+	<br>
+	`;
+	properties.innerHTML+=property_select_option;
+}
+
+function generate_query(){
+	
+	var block=document.getElementById("generated_query");
+	var word_prop_list=document.getElementsByClassName("word_property_list");
+	
+	query="";
+
+	for(i=0;i<word_prop_list.length;i++){
+		
+		var properties=word_prop_list[i].getElementsByClassName("property_name");
+		var properties_value=word_prop_list[i].getElementsByClassName("property_value");
+		
+		query+=" [ "
+		
+		for(j=0;j<properties.length;j++){
+			if(j!=0){
+				query+=" & ";
+			}
+			query+=properties[j].value+"= \""+properties_value[j].value+"\"";
+		}
+		query+=" ] "
+	}
+	block.innerHTML=query;
+}
