9d903655773d796c45a923589ee619cfd7ab066a
sagar-sehgal <sagar.r16@iiits.in>
2019-06-04 12:23:38 +0530

    DOM reconstruction issue solved

--- js/index.js
+++ js/index.js
@@ -1,47 +1,81 @@
 var counter=1;
 function add_word(button){
 	window.counter+=1;
-	word=`
-	<div class="word card">
-		<div class="card-header">
-			Word-${window.counter}
-		</div>
-		<div class="word_property_list card-body" style="overflow: auto;height: 20vh;">			
-			<div class="card" style="padding: 1%;width: 20vw; margin: 1%;float: left;">	
-				<select class="property_name" style="width: 100%;">
-					<option value="None" selected disabled>Feature</option>
-					<option value="conll:FORM">conll:FORM</option>
-					<option value="conll:LEMMA">conll:LEMMA</option>
-					<option value="conll:UPOSTAG">conll:UPOSTAG</option>
-					<option value="conll:XPOSTAG">conll:XPOSTAG</option>
-					<option value="conll:FEATS">conll:FEATS</option>
-					<option value="conll:DEPREL">conll:DEPREL</option>	
-				</select>
-				<input type="text" class="property_value" style="width: 100%;">
-			</div>	
-		</div> 
-		<button type="submit" onclick="add_property(this)">Add Property</button>
-	</div>
-	`;
-	document.getElementById('word_list').innerHTML+=word;
+
+	main_div=document.createElement('div');
+	main_div.className="word card";
+	div1=document.createElement('div');
+	div1.className="card-header";
+	div1.appendChild(document.createTextNode("Word-"+window.counter));
+	div2=document.createElement('div');
+	div2.className="word_property_list card-body";
+	div2.style="overflow: auto;height: 20vh;";
+	div21=document.createElement("div");
+	div21.className="card";
+	div21.style="padding: 1%;width: 20vw; margin: 1%;float: left;";
+	select=document.createElement("select");
+	select.className="property_name";
+	select.style="width: 100%;";
+	options=["FORM","LEMMA","UPOSTAG","XPOSTAG","FEATS","DEPREL"];
+	x=document.createElement("option");
+	x.text="Feature";
+	x.value="None";
+	x.selected=true;
+	x.disabled=true;
+	select.appendChild(x);
+	for(i=0;i<options.length;i++){
+		x=document.createElement("option");
+		x.text=options[i];
+		x.value="conll:"+options[i];
+		select.appendChild(x);
+	}
+	input=document.createElement("input");
+	input.type="text";
+	input.class="property_value";
+	input.style="width:100%;";
+	div21.appendChild(select);
+	div21.appendChild(input);
+	div2.appendChild(div21);
+	button=document.createElement("button");
+	button.type="submit";
+	button.setAttribute("onclick","add_property(this)");
+	button.appendChild(document.createTextNode("Add Property"));
+	main_div.appendChild(div1);
+	main_div.appendChild(div2);
+	main_div.appendChild(button);
+	
+	document.getElementById('word_list').appendChild(main_div);
 }
 function add_property(block){
 	var properties = block.parentElement.getElementsByClassName("word_property_list")[0];
-	var property_select_option=`
-	<div class="card" style="padding: 1%;width: 20vw; margin: 1%;float: left;">	
-		<select class="property_name" style="width: 100%;">
-			<option value="None" selected disabled>Feature</option>
-			<option value="conll:FORM">conll:FORM</option>
-			<option value="conll:LEMMA">conll:LEMMA</option>
-			<option value="conll:UPOSTAG">conll:UPOSTAG</option>
-			<option value="conll:XPOSTAG">conll:XPOSTAG</option>
-			<option value="conll:FEATS">conll:FEATS</option>
-			<option value="conll:DEPREL">conll:DEPREL</option>	
-		</select>
-		<input type="text" class="property_value" style="width: 100%;">
-	</div>
-	`;
-	properties.innerHTML+=property_select_option;
+	
+	div=document.createElement("div");
+	div.className="card";
+	div.style="padding: 1%;width: 20vw; margin: 1%;float: left;";
+	select=document.createElement("select");
+	select.className="property_name";
+	select.style="width: 100%;";
+	options=["FORM","LEMMA","UPOSTAG","XPOSTAG","FEATS","DEPREL"];
+	x=document.createElement("option");
+	x.text="Feature";
+	x.value="None";
+	x.selected=true;
+	x.disabled=true;
+	select.appendChild(x);
+	for(i=0;i<options.length;i++){
+		x=document.createElement("option");
+		x.text=options[i];
+		x.value="conll:"+options[i];
+		select.appendChild(x);
+	}
+	input=document.createElement("input");
+	input.type="text";
+	input.class="property_value";
+	input.style="width:100%;";
+	div.appendChild(select);
+	div.appendChild(input);
+
+	properties.appendChild(div);
 }
 
 function generate_query(){
