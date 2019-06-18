cbc2d1d31c5740a286f068ae10e1172052de3ed3
sagar-sehgal <sagar.r16@iiits.in>
2019-06-18 22:09:09 +0530

    dependency added

--- js/index.js
+++ js/index.js
@@ -17,16 +17,26 @@ function add_word(button){
 	button1.type="submit";
 	button1.setAttribute("onclick","delete_word(this)");
 	button1.appendChild(document.createTextNode("Delete Word"));
+	button1.className="btn btn-danger";
 	button1.style="float: right;";
 
 	button2=document.createElement("button");
 	button2.type="submit";
 	button2.setAttribute("onclick","add_property(this)");
 	button2.appendChild(document.createTextNode("Add Property"));
+	button2.className="btn btn-primary";
 	button2.style="float: left;";
 	
+	// <input type="text" name="word_name" class="form-control" style="float: right; width: 25%;margin: 0% 1%;" placeholder="Word Variable">
+	var_input=document.createElement("input");
+	var_input.type="text";
+	var_input.className="form-control";
+	var_input.style="width:30%;margin: 0% 1%;float:right;";
+	var_input.placeholder="Variable Name";
+
 	div1.appendChild(button1);
 	div1.appendChild(button2);
+	div1.appendChild(var_input);
 
 	main_div.appendChild(div1);
 	main_div.appendChild(div2);
@@ -40,17 +50,17 @@ function add_property(block){
 		
 	div=document.createElement("div");
 	div.className="card";
-	div.style="min-width: 15vw; width:15vw; margin:1%; padding:1%;display:inline-block;";
+	div.style="min-width: 20vw; width:20vw; margin: 1%;";
 	// min-width: 10vw; min-height: 10vh; margin:1%;
 	div_in=document.createElement("div");
-	div_in.className="inside_card";
+	div_in.className="inside_card card-body";
 	div_in.style="width:100%;margin: 1%;"
 
 	select1=document.createElement("select");
 	select1.className="property_name";
 	select1.style="width: 100%;";
 	
-	options1=["FORM","LEMMA","UPOSTAG","XPOSTAG","FEATS","DEPREL"];
+	options1=["ID","FORM","LEMMA","UPOSTAG","XPOSTAG","FEATS","DEPREL",];
 	x=document.createElement("option");
 	x.text="Feature";
 	x.value="None";
@@ -88,14 +98,22 @@ function add_property(block){
 	or_button.type="submit";
 	or_button.setAttribute("onclick","or_property(this)");
 	or_button.appendChild(document.createTextNode("OR"));
+	or_button.className="btn btn-primary";
 	or_button.style="width:40%;float:left;margin:1%;";
 
 	del_button=document.createElement("button");
 	del_button.type="submit";
 	del_button.setAttribute("onclick","delete_property(this)");
 	del_button.appendChild(document.createTextNode("Delete"));
+	del_button.className="btn btn-danger";
 	del_button.style="width:40%;float:right;margin:1%;";
 	
+	div_out=document.createElement("div");
+	div_out.className="card-header header_card";
+	// div_out.style="width:100%;margin: 1%;"
+
+	div_out.appendChild(or_button);
+	div_out.appendChild(del_button);
 
 	div_in.appendChild(select1);
 	div_in.appendChild(select2);
@@ -106,9 +124,9 @@ function add_property(block){
 	// and_box.style="padding:1%;margin: 1%;float: left;display: inline-block;";
 	// and_box.appendChild(document.createTextNode("AND"));
 
+	div.appendChild(div_out);
 	div.appendChild(div_in);
-	div.appendChild(or_button);
-	div.appendChild(del_button);
+	// div.appendChild(del_button);
 	
 	if(properties.childElementCount>0){
 		and=document.createElement("div");
@@ -124,7 +142,7 @@ function add_property(block){
 
 function generate_query(){
 	
-	var block=document.getElementById("generated_query");
+	var block=document.getElementById("cqp");
 	var word_prop_list=document.getElementsByClassName("word_property_list");
 	
 	query="";
@@ -148,7 +166,7 @@ function generate_query(){
 				if(name!="None" && value!=""){
 					if(has_info==true)
 						query_in+=" | "
-					query_in+=name;
+					query_in+="conll:"+name;
 					has_info=true;
 					x=true;
 					if(rel.classList.contains("fa-equals"))
@@ -172,7 +190,7 @@ function generate_query(){
 }
 
 function or_property(block){
-	var prop=block.parentElement.getElementsByClassName("inside_card")[0];
+	var prop=block.parentElement.parentElement.getElementsByClassName("inside_card")[0];
 
 	or=document.createElement("div");
 	or.appendChild(document.createTextNode("--OR--"));
@@ -217,7 +235,7 @@ function delete_word(block){
 }
 
 function delete_property(block){
-	var prop=block.parentElement;
+	var prop=block.parentElement.parentElement;
 	// if not the first child of the parent
 	if(prop.previousSibling!=null){
 		prop.previousSibling.remove();
@@ -233,3 +251,76 @@ function inverse(sign){
 		sign.classList.replace("fa-not-equal","fa-equals");
 	}
 }
+
+function delete_dependency(block){
+	var dependency=block.parentElement.parentElement;
+	dependency.remove();
+}
+
+function add_dependency(button){
+	// window.counter+=1;
+
+	main_div=document.createElement('div');
+	main_div.className="dependency card";
+	
+	div1=document.createElement('div');
+	div1.className="card-header";
+	
+	div2=document.createElement('div');
+	div2.className="card-body";
+	div2.style="display:flex; align-items: center; justify-content: center;";
+	
+	button1=document.createElement("button");
+	button1.type="submit";
+	button1.setAttribute("onclick","delete_dependency(this)");
+	button1.appendChild(document.createTextNode("Delete Dependency"));
+	button1.className="btn btn-danger";
+	button1.style="float: right;";
+
+	div1.appendChild(button1)
+
+	// input1
+	input1=document.createElement("input");
+	input1.type="text";
+	input1.className="word_left form-control";
+	input1.style="width:30%;margin: 1%;";
+	input1.placeholder="Left Variable";
+
+
+	// select1
+	select1=document.createElement("select");
+	select1.className="dependency_type";
+	select1.style="width: 40%;margin: 0% 1%;";
+	// options1=["nextWord"];
+	x=document.createElement("option");
+	x.text="Next Word";
+	x.value="nextWord";
+	x.selected=true;
+	select1.appendChild(x);
+	
+	x=document.createElement("option");
+	x.text="HEAD";
+	x.value="HEAD";
+	select1.appendChild(x);
+	
+
+	// input2
+	input2=document.createElement("input");
+	input2.type="text";
+	input2.className="word_right form-control";
+	input2.style="width:30%;margin: 0% 1%;";
+	input2.placeholder="Right Variable";
+
+	
+	div2.appendChild(input1);
+	div2.appendChild(select1);
+	div2.appendChild(input2);
+	
+
+	main_div.appendChild(div1);
+	main_div.appendChild(div2);
+	
+	document.getElementById('dependency_list').appendChild(main_div);
+
+}
+
